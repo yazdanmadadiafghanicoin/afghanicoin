2e1cbb9f08ae1d04eb19e84ffff90d996dc40ddc
yazdanmadadiafghanicoin <yazdanmadadi@gmail.com>
2026-09-24 14:02:24 +0430

    Expand API handler with user and upgrade endpoints
    Signed-off-by: yazdanmadadiafghanicoin <yazdanmadadi@gmail.com>

--- api/index.js
+++ api/index.js
@@ -1,7 +1,48 @@
 export default function handler(req, res) {
-  res.status(200).json({
-    success: true,
-    message: "Afghani Coin Backend is running!",
-    coin: "AFC"
-  });
-                       }
+  const url = new URL(req.url, `http://${req.headers.host}`);
+
+  if (req.method === "GET" && url.pathname === "/api") {
+    return res.status(200).json({
+      success: true,
+      message: "Afghani Coin Backend is running!",
+      coin: "AFC",
+      balance: 0,
+      energy: 100,
+      level: 1,
+      power: 1
+    });
+  }
+
+  if (req.method === "GET" && url.pathname === "/api/user") {
+    return res.status(200).json({
+      success: true,
+      balance: 0,
+      energy: 100,
+      level: 1,
+      power: 1
+    });
+  }
+
+  if (req.method === "POST" && url.pathname === "/api/mine") {
+    return res.status(200).json({
+      success: true,
+      message: "Mining successful!",
+      mined: 1,
+      balance: 1,
+      energy: 99
+    });
+  }
+
+  if (req.method === "POST" && url.pathname === "/api/upgrade") {
+    return res.status(200).json({
+      success: true,
+      message: "Upgrade successful!",
+      balance: 0,
+      energy: 100,
+      level: 2,
+      power: 2
+    });
+  }
+
+  return res.status(404).json({
+   
