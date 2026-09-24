260e3f77647ecbeceab749b7dc09dcc4994c8309
yazdanmadadiafghanicoin <yazdanmadadi@gmail.com>
2026-09-24 12:31:19 +0430

    Update bot.py
    Signed-off-by: yazdanmadadiafghanicoin <yazdanmadadi@gmail.com>

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import time
 TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 API = f"https://api.telegram.org/bot{TOKEN}"
 
-WEB_APP_URL = "https://yazdanmadadiafghanicoin.github.io/afghanicoin/?v=4"
+WEB_APP_URL = "https://yazdanmadadiafghanicoin.github.io/afghanicoin/?v=9999"
 
 
 def telegram(method, data=None):
