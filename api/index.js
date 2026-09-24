f17be072c7f3d4503d024a1ad429796916a90102
yazdanmadadiafghanicoin <yazdanmadadi@gmail.com>
2026-09-24 13:34:11 +0430

    گنخه
    Signed-off-by: yazdanmadadiafghanicoin <yazdanmadadi@gmail.com>

--- api/index.js
+++ api/index.js
@@ -1,1 +1,7 @@
-
+export default function handler(req, res) {
+  res.status(200).json({
+    success: true,
+    message: "Afghani Coin Backend is running!",
+    coin: "AFC"
+  });
+                       }
